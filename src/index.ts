1dd61e215ffb9170fd9d52d660f41b1073a79edb
Fabian Vilers <fabian.vilers@dev-one.com>
2022-06-17 06:40:23 +0000

    Add program commands

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,48 @@
-console.log("Hello");
+#! /usr/bin/env node
+
+import { Command } from "commander";
+
+const program = new Command();
+program.name("task").description("A simple command line to-do manager");
+
+program
+  .command("add")
+  .description("add a task")
+  .argument("<task>", "The task")
+  .action((task) => {
+    // TODO: create the task
+  });
+
+program
+  .command("list")
+  .description("list tasks")
+  .option("-a, --all", "Include done tasks")
+  .action((options) => {
+    // TODO: get tasks and list them
+  });
+
+program
+  .command("done")
+  .description("mark a task as done")
+  .argument("<id>", "The task ID")
+  .action((id) => {
+    // TODO: mark task as done
+  });
+
+program
+  .command("undone")
+  .description("mark a task as undone")
+  .argument("<id>", "The task ID")
+  .action((id) => {
+    // TODO: reset task to undone
+  });
+
+program
+  .command("delete")
+  .description("delete a task")
+  .argument("<id>", "The task ID")
+  .action((id) => {
+    // TODO: delete task
+  });
+
+program.parse();
